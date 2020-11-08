79e0cf777cfae9dea92c6ee2491c74c257bdc393
fangnm <fangnm1940@gmail.com>
2020-11-08 10:34:05 +0700

    Initial commit

---
+++ app/src/main/java/th/ac/su/cp/speedrecords/MainActivity.java
@@ -0,0 +1,26 @@
+package th.ac.su.cp.speedrecords;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button learnButton  = findViewById(R.id.Addbutton);
+        learnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent  = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
